8ec58c83a4f839f530dd050b0c3e9a0af5dd84ef
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-04-23 20:26:46 +0200

    Updates

---
+++ precache-manifest.ef1c0debf2d6d8c764af015caaddb436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9588bf25e4b4c623d5c75f181f860a68",
+    "url": "/todo-react-app/static/media/check.9588bf25.svg"
+  },
+  {
+    "revision": "cd1261c6c332fce322077f35074f65f4",
+    "url": "/todo-react-app/static/media/cancel.cd1261c6.svg"
+  },
+  {
+    "revision": "5fd7fe22f3e7a4aea29a",
+    "url": "/todo-react-app/static/js/runtime~main.7e9341e7.js"
+  },
+  {
+    "revision": "2145ba0d30971aac47e8",
+    "url": "/todo-react-app/static/js/main.f4d0bb3a.chunk.js"
+  },
+  {
+    "revision": "c42fe381f022d7354d27",
+    "url": "/todo-react-app/static/js/2.b48df737.chunk.js"
+  },
+  {
+    "revision": "2145ba0d30971aac47e8",
+    "url": "/todo-react-app/static/css/main.38e4d2f2.chunk.css"
+  },
+  {
+    "revision": "23a31a46f34be87ccf4059cad74d35d1",
+    "url": "/todo-react-app/index.html"
+  }
+];
